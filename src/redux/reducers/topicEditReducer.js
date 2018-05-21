5a9f69aee8fc660b1971d6f8cee86ba248de892f
Justin <jmpeterson9891@gmail.com>
2018-05-21 11:49:11 -0500

    Created reducer and saga for featured topic for the landing page

--- src/redux/reducers/topicEditReducer.js
+++ src/redux/reducers/topicEditReducer.js
@@ -1,30 +1,61 @@
 import { combineReducers } from 'redux';
 
-const newTopic = (state = [], action) => {
+//featuredTopicPage contains the information needed for the featured topic
+//on the Topic Page
+const featuredTopicPage = (state = [], action) => {
 
-    //sets state of newTopic to an array of objects where
-    //each object is a section of the topic edit page e.g. one object would be the Topic Title,
-    //another object would be Topic Summary, etc.
-    if (action.type === 'SET_NEW_FEATURED_TOPIC'){
+    //sets state of featuredTopicPage to an array of objects where each object is a section
+    //of the topic edit page e.g. one object would be the Topic Title, another
+    //object would be Topic Summary, etc.
+    if (action.type === 'SET_FEATURED_TOPIC_TOPIC_PAGE'){
         return action.payload
     }
 
-    //if action 'SET_NEW_FEATURED_TOPIC' is not received, state is set to its default empty array
+    //if action 'SET_FEATURED_TOPIC_TOPIC_PAGE' is not received, state is set to its 
+    //default empty array
+    return state
+}
+
+//featuredLandingPage contains the information needed to display the featured topic
+//the featured topic on the landing page
+const featuredLandingPage = (state = [], action) => {
+
+    //sets state of featuredLandingPage to an array of objects where each object is a section
+    //of the featured topic on the landing page e.g. contributor names, contributor bios, etc.
+    if(action.type === 'SET_FEATURED_TOPIC_LANDING_PAGE'){
+        return action.paylod
+    }
+
+    //if action 'SET_FEATURED_TOPIC_LANDING_PAGE' is not received, state is set to its 
+    //default empty array
     return state
 }
 
 const archivedTopics = (state = [], action) => {
 
-    //sets state of archivedTopics to an array of objects where those objects are all archived topics
+    //sets state of archivedTopics to an array of objects where those objects are 
+    //all archived topics
     if(action.type === 'SET_ARCHIVED_TOPICS'){
         return action.payload
     }
 
-    //if action 'SET_ARCHIVED_TOPCS' is not received, state is set to its default empty array
+    //if action 'SET_ARCHIVED_TOPCS' is not received, state is set to its default 
+    //empty array
+    return state
+} 
+
+const comments = (state = [], action) => {
+    if(action.type === 'SET_COMMENTS'){
+        return action.payload
+    }
+
+    //if action 'SET_COMMENTS' is not received, state is set to its default empty array
     return state
 }
 
 export default combineReducers({
-    newTopic,
-    archivedTopics
+    featuredTopicPage,
+    featuredLandingPage,
+    archivedTopics,
+    comments
 })
--- src/redux/sagas/topicSaga.js
+++ src/redux/sagas/topicSaga.js
@@ -1,18 +1,19 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios'
 
-function* fetchNewTopic(action){
+//gets the featured topic to be displayed on the topic page from the database
+function* fetchFeaturedTopicPage(action){
     try{
 
-        //topicResponse sends get request to router '/api/topic/featured' and 
-        //receives back the topic admin wants to be the featured topic and ids and stores
-        //it in topicResponse.data
-        const topicResponse = yield call(axios.get, '/api/topic/featured')
+        //topicResponse sends get request to router '/api/topic/featuredtopic' and 
+        //receives back the topic admin wants to be the featured topic for the topic
+        //page and stores it in topicResponse.data
+        const topicResponse = yield call(axios.get, '/api/topic/featuredtopic')
 
-        //sends new featured topic to newTopic reducer via action 'SET_NEW_FEATURED_TOPIC'
+        //sends new featured topic to newTopic reducer via action 'SET_FEATURED_TOPIC_TOPIC_PAGE'
         //and payload topicResponse.data
         yield put({
-            type: 'SET_NEW_FEATURED_TOPIC',
+            type: 'SET_FEATURED_TOPIC_TOPIC_PAGE',
             payload: topicResponse.data
         })
 
@@ -24,6 +25,7 @@ function* fetchNewTopic(action){
     }
 }
 
+//gets all of the archived topics for displaying them on the landing page
 function* fetchArchivedTopics(action){
     try{
 
@@ -46,8 +48,33 @@ function* fetchArchivedTopics(action){
     }
 }
 
+//gets the featured topic to be displayed on the landing page from the database
+function* fetchFeaturedTopicLanding(action){
+    try{
+
+        //topicResponse sends get request to router '/api/topic/featuredlanding' and 
+        //receives back the topic admin wants to be the featured topic on the landing
+        //page and stores it in topicResponse.data
+        const topicResponse = yield call(axios.get, '/api/topic/featuredlanding')
+
+        //sends new featured topic to newTopic reducer via action 'SET_NEW_FEATURED_TOPIC'
+        //and payload topicResponse.data
+        yield put({
+            type: 'SET_FEATURED_TOPIC_LANDING_PAGE',
+            payload: topicResponse.data
+        })
+
+    //if there is an error in sending get request to router, the error
+    //will display in the console log
+    }catch(error){
+        console.log('Error in getting topic: ', error);
+        
+    }
+}
+
 function* factionSaga() {
-    yield takeLatest('FETCH_NEW_TOPIC', fetchNewTopic)
+    yield takeLatest('FETCH_NEW_TOPIC_TOPIC_PAGE', fetchFeaturedTopicPage)
+    yield takeLatest('FETCH_NEW_TOPIC_LANDING_PAGE', fetchFeaturedTopicLanding)
     yield takeLatest('FETCH_ARCHIVED_TOPICS', fetchArchivedTopics)
   }
 
