a744a75499329faa7a37cd9d648b2da08763e59b
Atticus Pomerantz <rapomerantz@gmail.com>
2018-05-21 17:13:00 -0500

    Topic Manage ready for redux

---
+++ src/components/Admin/TopicManage/NewTopicPanel.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import { Link } from 'react-router-dom';
+import { Panel, Button, ButtonGroup, ButtonToolbar, Glyphicon } from 'react-bootstrap'; 
+
+
+class NewTopicPanel extends Component {
+  render() {
+    return (
+      <div>
+
+
+        
+      </div>
+    )
+  }
+}
+
+export default NewTopicPanel; 
