71938e675d511f88d8e6845326638b339964c434
Justin <jmpeterson9891@gmail.com>
2018-05-23 13:38:01 -0500

    started post route for big object

--- server/routes/topic.router.js
+++ server/routes/topic.router.js
@@ -37,95 +37,20 @@ router.get('/alltopics', (req, res) => {
     // }
 });
 
-//gets all key claims from database
-router.get('/allkeyclaims', (req, res) => {
+//gets the featured topic from database
+//for explanation of any variables or methods in the following get request, see
+//router.get('/alltopics') as the same variables and methods are used there
+router.get('/featuredtopic', (req, res) => {
 
-    //see comment from router.get('/alltopics') for explanation of 
-    //if(req.isAuthenticated())
     // if(req.isAuthenticated()){
 
-    //queryText is the query text that will get all of the key claims from 
-    //the key_claim table in the database
     const queryText = `;`
 
-    //pool.query is the method that sends the queryText to the database and 
-    //stores the results in the variable result
     pool.query(queryText).then((result) => {
-    
-    //all of the key claims are stored in result.rows; therefore we will send back
-    //result.rows
         res.send(result.rows)
 
-    //if there was an error in getting the key claims from the database,
-    //the error will be displayed in the console log
     }).catch((error) => {
-        console.log('Error in getting key claims: ', error);
-        
-    })
-    // } else{
-
-    //     //if req.isAuthenticated() is false, the forbidden error will appear
-    //     //on the webpage
-    //     res.sendStatus(403)
-    // }
-});
-
-//gets all streams from database
-router.get('/allstreams', (req, res) => {
-
-    //see comment from router.get('/alltopics') for explanation of 
-    //if(req.isAuthenticated())
-    // if(req.isAuthenticated()){
-
-    //queryText is the query text that will get all of the streams from 
-    //the stream table in the database
-    const queryText = `;`
-
-    //pool.query is the method that sends the queryText to the database and 
-    //stores the results in the variable result
-    pool.query(queryText).then((result) => {
-    
-    //all of the streams are stored in result.rows; therefore we will send back
-    //result.rows
-        res.send(result.rows)
-
-    //if there was an error in getting the streams from the database,
-    //the error will be displayed in the console log
-    }).catch((error) => {
-        console.log('Error in getting streams: ', error);
-        
-    })
-    // } else{
-
-    //     //if req.isAuthenticated() is false, the forbidden error will appear
-    //     //on the webpage
-    //     res.sendStatus(403)
-    // }
-});
-
-//gets all contributors from database
-router.get('/allcontributors', (req, res) => {
-
-    //see comment from router.get('/alltopics') for explanation of 
-    //if(req.isAuthenticated())
-    // if(req.isAuthenticated()){
-
-    //queryText is the query text that will get all of the contributors from 
-    //the contributor table in the database
-    const queryText = `SELECT * FROM "contributor";`
-
-    //pool.query is the method that sends the queryText to the database and 
-    //stores the results in the variable result
-    pool.query(queryText).then((result) => {
-    
-    //all of the contributors are stored in result.rows; therefore we will send back
-    //result.rows
-        res.send(result.rows)
-
-    //if there was an error in getting the topics from the database,
-    //the error will be displayed in the console log
-    }).catch((error) => {
-        console.log('Error in getting contributors: ', error);
+        console.log('Error in getting loves: ', error);
         
     })
     // } else{
@@ -141,7 +66,61 @@ router.get('/allcontributors', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post('/newtopic', (req, res) => {
+
+    if(req.isAuthenticated){
+         //game is the newInput object from state in GameInfo.js
+         const topic = req.body;
+
+         (async () => {
+             //client does not allow the program to proceed until it is connected to the database
+             const client = await pool.connect();
+ 
+             try{
+                 await client.query('BEGIN');
+ 
+                 //creates an entry in the topic table in the database
+                 let queryText = `INSERT INTO "topic" ("topic_title", "premise", "common_ground", "archive_summary") 
+                 VALUES($1, $2, $3, $4)  RETURNING "id";`;
+                 const topicResult = await client.query(queryText, [topic.topicTitle, topic.topicPremise, topic.topicCommonGround, topic.topicSummary]);
+ 
+                 //the id of the topic that was created in topicResult
+                 const topicId = topicResult.rows[0].id
+ 
+                 //text for posting gameinfo to the database
+                 queryText = `INSERT INTO "" ("user_id", "game_id", "player_name",
+                 "faction1", "faction2", "points", "rank", "bases", "comments", "admin") 
+                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`;
+ 
+                 //takes user id, game id, and the property values from the variable game
+                 //and inputs them into the VALUES ($1, $2, $3,...) of the queryText
+                 //client.query then posts the info to the database
+                 const result = await client.query(queryText, [req.user.id, gameId, game.playerName,
+                 game.factionArray[0], game.factionArray[1], game.points, game.rank, game.bases,
+                 game.comments, req.user.id])
+                 await client.query('COMMIT');
+                 res.sendStatus(201);
+ 
+             } catch (e) {
+ 
+                 //checks for errors at any point within the try block; if errors are found,
+                 //all the data is cleared to prevent data corruption
+                 console.log('ROLLBACK', e);
+                 await client.query('ROLLBACK');
+                 throw e;
+             } finally {
+ 
+                 //allows res.sendStatus(201) to be sent
+                 client.release();
+             }
+ 
+             //if an error occurs in posting the game info to the database, the error will
+             //appear in the console log
+         })().catch((error) => {
+             console.log('CATCH', error);
+             res.sendStatus(500);
+         })
+    }
 
 });
 
@@ -151,13 +130,18 @@ router.post('/', (req, res) => {
 //TOGGLES PUBLISHED STATUS IN TOPIC TABLE
 router.put('/togglePublished', (req, res) => {
     console.log('in /api/topics/togglePublished', req.body);
+
+    //topicId contains the id of the topic whose status of published or not published
+    //we want to change
     let topicId = req.body.id; 
     let queryText = `UPDATE topic SET published = NOT published WHERE id = $1;`
+
     pool.query(queryText, [topicId])
     .then((result)=> {
         console.log('successful PUT /api/topic/togglePublished');
         res.sendStatus(200);
     })
+
     .catch((err)=> {
         console.log('error in PUT /api/topic/togglePublished');
         res.sendStatus(500); 
@@ -168,21 +152,34 @@ router.put('/togglePublished', (req, res) => {
 //TOGGLES FEATURED STATUS IN TOPIC TABLE (SETS ALL TO UNFEATURED, SETS SELECTED TO FEATURED)
 router.put('/toggleFeatured', (req, res) => {
     console.log('in /api/topics/toggleFeatured', req.body);
+
+    //topicId contains the id of the topic whose status of featured
+    //we want to change
     let topicId = req.body.id; 
+
+    //since we can only have one topic be the featured topic, we will start off
+    //by setting the featured status of every topic to false via firstQueryText;
+    //then we will change the status of the topic we want to be the featured topic
+    //to true via the secondQueryText
     let firstQueryText = `UPDATE topic SET featured = FALSE;`
     let secondQueryText = `UPDATE topic SET featured = TRUE WHERE id = $1;`
+
     pool.query(firstQueryText)
     .then((result)=> {
+
+        //topicId tells the topic table which topic we want to change to the featured topic
         pool.query(secondQueryText, [topicId])
         .then((result) => {
             console.log('successful PUT /api/topic/toggleFeatured');
             res.sendStatus(200); 
         })
+
         .catch((err)=> {
             console.log('error in PUT /api/topic/toggleFeatured');
             res.sendStatus(500); 
         })
     })
+
     .catch((err)=> {
         console.log('error in PUT /api/topic/toggleFeatured');
         res.sendStatus(500); 
--- src/redux/reducers/topicEditReducer.js
+++ src/redux/reducers/topicEditReducer.js
@@ -17,92 +17,6 @@ const allTopics = (state = [], action) => {
     return state
 }
 
-//allKeyClaims contains all key claims from the database
-const allKeyClaims = (state = [], action) => {
-
-    //sets state of allKeyClaims to an array of objects where each object is a key claim
-    //with topic id, contributor id, claim text, and order id
-    if (action.type === 'SET_ALL_KEY_CLAIMS'){
-        return action.payload
-    }
-
-    //if action 'SET_ALL_KEY_CLAIMS' is not received, state is set to its 
-    //default empty array
-    return state
-}
-
-//allStreams contains all streams from the database
-const allStreams = (state = [], action) => {
-
-    //sets state of allStreams to an array of objects where each object is a stream
-    //with key claim id, contributor id, stream text, and evidence text
-    if (action.type === 'SET_ALL_STREAMS'){
-        return action.payload
-    }
-
-    //if action 'SET_ALL_STREAMS' is not received, state is set to its 
-    //default empty array
-    return state
-}
-
-//allContributors contains all contributors from the database
-const allContributors = (state = [], action) => {
-
-    //sets state of allContributors to an array of objects where each object is a contributor
-    //with their first name, last name, bio, and photo url
-    if (action.type === 'SET_ALL_CONTRIBUTORS'){
-        return action.payload
-    }
-
-    //if action 'SET_ALL_CONTRIBUTORS' is not received, state is set to its 
-    //default empty array
-    return state
-}
-
-//allProposals contains all proposals from the database
-const allProposals = (state = [], action) => {
-
-    //sets state of allProposals to an array of objects where each object is a proposal
-    //with the topic id, contributor id, and the proposal text
-    if (action.type === 'SET_ALL_PROPOSALS'){
-        return action.payload
-    }
-
-    //if action 'SET_ALL_PROPOSALS' is not received, state is set to its 
-    //default empty array
-    return state
-}
-
-//allLikes contains all likes from the database
-const allLikes = (state = [], action) => {
-
-    //sets state of allLikes to an array of objects where each object is a like
-    //with the user id and the id of the item liked e.g. key claim id, stream id,
-    //id of a comment on a key claim, etc.
-    if (action.type === 'SET_ALL_LIKES'){
-        return action.payload
-    }
-
-    //if action 'SET_ALL_LIKES' is not received, state is set to its 
-    //default empty array
-    return state
-}
-
-//allLoves contains all loves from the database
-const allLoves = (state = [], action) => {
-
-    //sets state of allLoves to an array of objects where each object is a love
-    //with the user id and the id of the item loved e.g. key claim id, stream id,
-    //id of a comment on a key claim, etc.
-    if (action.type === 'SET_ALL_LOVES'){
-        return action.payload
-    }
-
-    //if action 'SET_ALL_LOVES' is not received, state is set to its 
-    //default empty array
-    return state
-}
-
 //featuredTopicPage contains the information needed for the featured topic
 //on the Topic Page
 const featuredTopicPage = (state = [], action) => {
@@ -190,12 +104,6 @@ const commentsStream = (state = [], action) => {
 
 export default combineReducers({
     allTopics,
-    allKeyClaims,
-    allStreams,
-    allContributors,
-    allProposals,
-    allLikes,
-    allLoves,
     featuredTopicPage,
     featuredLandingPage,
     archivedTopics,
