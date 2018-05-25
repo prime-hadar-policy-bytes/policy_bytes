2f9b9a8d73ebdf4e5d6dd57d37829c64c8d99052
David Kesler <thedavidkesler@gmail.com>
2018-05-25 14:07:12 -0500

    sql-get-updates

--- server/routes/topic.router.js
+++ server/routes/topic.router.js
@@ -18,16 +18,16 @@ router.get('/alltopics', (req, res) => {
     //pool.query is the method that sends the queryText to the database and 
     //stores the results in the variable result
     pool.query(queryText).then((result) => {
-    
-    //all of the topics are stored in result.rows; therefore we will send back
-    //result.rows
+
+        //all of the topics are stored in result.rows; therefore we will send back
+        //result.rows
         res.send(result.rows)
 
-    //if there was an error in getting the topics from the database,
-    //the error will be displayed in the console log
+        //if there was an error in getting the topics from the database,
+        //the error will be displayed in the console log
     }).catch((error) => {
         console.log('Error in getting topics: ', error);
-        
+
     })
     // } else{
 
@@ -51,7 +51,7 @@ router.get('/featuredtopic', (req, res) => {
 
     }).catch((error) => {
         console.log('Error in getting loves: ', error);
-        
+
     })
     // } else{
 
@@ -66,127 +66,128 @@ router.get('/featuredtopic', (req, res) => {
 /**
  * POST route template
  */
-router.post('/newtopic', (req, res) => {
+// router.post('/newtopic', (req, res) => {
 
-    if(req.isAuthenticated){
-         //game is the newInput object from state in GameInfo.js
-         const topic = req.body;
 
-         (async () => {
-             //client does not allow the program to proceed until it is connected to the database
-             const client = await pool.connect();
- 
-             try{
-                 await client.query('BEGIN');
- 
-                 //creates an entry in the topic table in the database
-                 let queryText = `INSERT INTO "topic" ("topic_title", "premise", "common_ground", "archive_summary") 
-                 VALUES($1, $2, $3, $4)  RETURNING "id";`;
-                 const topicResult = await client.query(queryText, [topic.topicTitle, topic.topicPremise, topic.topicCommonGround, topic.topicSummary]);
- 
-                 //the id of the topic that was created in topicResult
-                 const topicId = topicResult.rows[0].id
- 
-                 //text for posting gameinfo to the database
-                 let queryText1 = `INSERT INTO "contributor" ("first_name", "last_name", "bio", "photo_url")
-                 VALUES($1, $2, $3, $4) RETURNING "id";`;
-                 const contributor1Result = await client.query(queryText1, [topic.contributor1FirstName, 
-                    topic.contributor1LastName, topic.contributor1Bio, topic.contributor1Photo]);
-                
-                const contributor1Id = contributor1Result.rows[0].id
+//     if(req.isAuthenticated){
+//          //game is the newInput object from state in GameInfo.js
+//          const topic = req.body;
 
-                let queryText2 = `INSERT INTO "contributor" ("first_name", "last_name", "bio", "photo_url")
-                 VALUES($1, $2, $3, $4) RETURNING "id";`;
-                 const contributor2Result = await client.query(queryText2, [topic.contributor2FirstName, 
-                    topic.contributor2LastName, topic.contributor2Bio, topic.contributor2Photo]);
-                
-                const contributor2Id = contributor2Result.rows[0].id
+//          (async () => {
+//              //client does not allow the program to proceed until it is connected to the database
+//              const client = await pool.connect();
 
-                let queryText3 = `INSERT INTO "proposal" ("topic_id", "contributor_id", "proposal") VALUES($1,
-                    $2, $3);`
-                    
-                 await client.query(queryText3, [topicId, contributor1Id, topic.proposal1])
+//              try{
+//                  await client.query('BEGIN');
 
-                 let queryText4 = `INSERT INTO "proposal" ("topic_id", "contributor_id", "proposal") VALUES($1,
-                    $2, $3);`
-                    
-                 await client.query(queryText4, [topicId, contributor2Id, topic.proposal2])
+//                  //creates an entry in the topic table in the database
+//                  let queryText = `INSERT INTO "topic" ("topic_title", "premise", "common_ground", "archive_summary") 
+//                  VALUES($1, $2, $3, $4)  RETURNING "id";`;
+//                  const topicResult = await client.query(queryText, [topic.topicTitle, topic.topicPremise, topic.topicCommonGround, topic.topicSummary]);
 
-                //key is each property in keyClaims e.g. 0:{topicId: 1, ...}, 1:{topicId: 2, ...}, ...
-                 for(key in topic.keyClaims){
-                    let claim_order = key;
-                     //keyData is the value of a property in the keyClaims object
-                    let keyData = topic.keyClaim[key]
-                    let keyClaimData = [];
-                    
-                     for(prop in keyData){
+//                  //the id of the topic that was created in topicResult
+//                  const topicId = topicResult.rows[0].id
 
-                        let keyDataProp = keyData[prop]
-                        keyClaimData.push(keyDataProp);
-                        //end for loop of for(prop in keyData)
+//                  //text for posting gameinfo to the database
+//                  let queryText1 = `INSERT INTO "contributor" ("first_name", "last_name", "bio", "photo_url")
+//                  VALUES($1, $2, $3, $4) RETURNING "id";`;
+//                  const contributor1Result = await client.query(queryText1, [topic.contributor1FirstName, 
+//                     topic.contributor1LastName, topic.contributor1Bio, topic.contributor1Photo]);
 
-                        let queryText5 = `INSERT INTO "key_claim" ("topic_id", "contributor_id", "claim", "claim_order")
-                 VALUES($1, $2, $3, $4) RETURNING "id";`;
-                        let contributor;
-                        if(keyClaimData[1] === 'contributor1'){
-                            contributor = contributor1Id
-                        }else{
-                            contributor = contributor2Id
-                        }
-                        const keyClaimResult = await client.query(queryText5, [topicId, contributor, keyClaimData[2], claim_order])
+//                 const contributor1Id = contributor1Result.rows[0].id
 
-                        const keyClaimId = keyClaimResult.rows[0].id
+//                 let queryText2 = `INSERT INTO "contributor" ("first_name", "last_name", "bio", "photo_url")
+//                  VALUES($1, $2, $3, $4) RETURNING "id";`;
+//                  const contributor2Result = await client.query(queryText2, [topic.contributor2FirstName, 
+//                     topic.contributor2LastName, topic.contributor2Bio, topic.contributor2Photo]);
 
-                        let streamData = keyClaimData[3]
-                        let streamClaimData = [];
+//                 const contributor2Id = contributor2Result.rows[0].id
 
-                        for(stream in streamData){
-                            let streamDataProp = streamData[stream]
-                            streamClaimData.push(streamDataProp)
-                        }
-                        let queryText6 = `INSERT INTO "stream" ("key_claim_id", "contributor_id", "text", "evidence")
-                        VALUES ($1, $2, $3, $4)`
-                        if(streamClaimData[0] === 'contributor1'){
-                            contributor = contributor1Id;
-                        }else{
-                            contributor = contributor2Id
-                        }
+//                 let queryText3 = `INSERT INTO "proposal" ("topic_id", "contributor_id", "proposal") VALUES($1,
+//                     $2, $3);`
 
-                        await client.query(queryText6, [keyClaimId, contributor, streamClaimData[1], streamClaimData[2]])
+//                  await client.query(queryText3, [topicId, contributor1Id, topic.proposal1])
 
-                     }
-                 }
+//                  let queryText4 = `INSERT INTO "proposal" ("topic_id", "contributor_id", "proposal") VALUES($1,
+//                     $2, $3);`
 
-                 const contributor2Result = await client.query(queryText2, [topic.keyClaims, 
-                    topic.contributor2LastName, topic.contributor2Bio, topic.contributor2Photo]);
-                
-                const contributor2Id = contributor2Result.rows[0].id
+//                  await client.query(queryText4, [topicId, contributor2Id, topic.proposal2])
 
-                 await client.query('COMMIT');
-                 res.sendStatus(201);
- 
-             } catch (e) {
- 
-                 //checks for errors at any point within the try block; if errors are found,
-                 //all the data is cleared to prevent data corruption
-                 console.log('ROLLBACK', e);
-                 await client.query('ROLLBACK');
-                 throw e;
-             } finally {
- 
-                 //allows res.sendStatus(201) to be sent
-                 client.release();
-             }
- 
-             //if an error occurs in posting the game info to the database, the error will
-             //appear in the console log
-         })().catch((error) => {
-             console.log('CATCH', error);
-             res.sendStatus(500);
-         })
-    }
+//                 //key is each property in keyClaims e.g. 0:{topicId: 1, ...}, 1:{topicId: 2, ...}, ...
+//                  for(key in topic.keyClaims){
+//                     let claim_order = key;
+//                      //keyData is the value of a property in the keyClaims object
+//                     let keyData = topic.keyClaim[key]
+//                     let keyClaimData = [];
 
-});
+//                      for(prop in keyData){
+
+//                         let keyDataProp = keyData[prop]
+//                         keyClaimData.push(keyDataProp);
+//                         //end for loop of for(prop in keyData)
+
+//                         let queryText5 = `INSERT INTO "key_claim" ("topic_id", "contributor_id", "claim", "claim_order")
+//                  VALUES($1, $2, $3, $4) RETURNING "id";`;
+//                         let contributor;
+//                         if(keyClaimData[1] === 'contributor1'){
+//                             contributor = contributor1Id
+//                         }else{
+//                             contributor = contributor2Id
+//                         }
+//                         const keyClaimResult = await client.query(queryText5, [topicId, contributor, keyClaimData[2], claim_order])
+
+//                         const keyClaimId = keyClaimResult.rows[0].id
+
+//                         let streamData = keyClaimData[3]
+//                         let streamClaimData = [];
+
+//                         for(stream in streamData){
+//                             let streamDataProp = streamData[stream]
+//                             streamClaimData.push(streamDataProp)
+//                         }
+//                         let queryText6 = `INSERT INTO "stream" ("key_claim_id", "contributor_id", "text", "evidence")
+//                         VALUES ($1, $2, $3, $4)`
+//                         if(streamClaimData[0] === 'contributor1'){
+//                             contributor = contributor1Id;
+//                         }else{
+//                             contributor = contributor2Id
+//                         }
+
+//                         await client.query(queryText6, [keyClaimId, contributor, streamClaimData[1], streamClaimData[2]])
+
+//                      }
+//                  }
+
+//                  const contributor2Result = await client.query(queryText2, [topic.keyClaims, 
+//                     topic.contributor2LastName, topic.contributor2Bio, topic.contributor2Photo]);
+
+//                 const contributor2Id = contributor2Result.rows[0].id
+
+//                  await client.query('COMMIT');
+//                  res.sendStatus(201);
+
+//              } catch (e) {
+
+//                  //checks for errors at any point within the try block; if errors are found,
+//                  //all the data is cleared to prevent data corruption
+//                  console.log('ROLLBACK', e);
+//                  await client.query('ROLLBACK');
+//                  throw e;
+//              } finally {
+
+//                  //allows res.sendStatus(201) to be sent
+//                  client.release();
+//              }
+
+//              //if an error occurs in posting the game info to the database, the error will
+//              //appear in the console log
+//          })().catch((error) => {
+//              console.log('CATCH', error);
+//              res.sendStatus(500);
+//          })
+//     }
+
+// });
 
 
 
@@ -197,19 +198,19 @@ router.put('/togglePublished', (req, res) => {
 
     //topicId contains the id of the topic whose status of published or not published
     //we want to change
-    let topicId = req.body.id; 
+    let topicId = req.body.id;
     let queryText = `UPDATE topic SET published = NOT published WHERE id = $1;`
 
     pool.query(queryText, [topicId])
-    .then((result)=> {
-        console.log('successful PUT /api/topic/togglePublished');
-        res.sendStatus(200);
-    })
+        .then((result) => {
+            console.log('successful PUT /api/topic/togglePublished');
+            res.sendStatus(200);
+        })
 
-    .catch((err)=> {
-        console.log('error in PUT /api/topic/togglePublished');
-        res.sendStatus(500); 
-    })
+        .catch((err) => {
+            console.log('error in PUT /api/topic/togglePublished');
+            res.sendStatus(500);
+        })
 })
 
 //WRITTEN BY ATTICUS
@@ -219,7 +220,7 @@ router.put('/toggleFeatured', (req, res) => {
 
     //topicId contains the id of the topic whose status of featured
     //we want to change
-    let topicId = req.body.id; 
+    let topicId = req.body.id;
 
     //since we can only have one topic be the featured topic, we will start off
     //by setting the featured status of every topic to false via firstQueryText;
@@ -229,73 +230,91 @@ router.put('/toggleFeatured', (req, res) => {
     let secondQueryText = `UPDATE topic SET featured = TRUE WHERE id = $1;`
 
     pool.query(firstQueryText)
-    .then((result)=> {
-
-        //topicId tells the topic table which topic we want to change to the featured topic
-        pool.query(secondQueryText, [topicId])
         .then((result) => {
-            console.log('successful PUT /api/topic/toggleFeatured');
-            res.sendStatus(200); 
+
+            //topicId tells the topic table which topic we want to change to the featured topic
+            pool.query(secondQueryText, [topicId])
+                .then((result) => {
+                    console.log('successful PUT /api/topic/toggleFeatured');
+                    res.sendStatus(200);
+                })
+
+                .catch((err) => {
+                    console.log('error in PUT /api/topic/toggleFeatured');
+                    res.sendStatus(500);
+                })
         })
 
-        .catch((err)=> {
+        .catch((err) => {
             console.log('error in PUT /api/topic/toggleFeatured');
-            res.sendStatus(500); 
+            res.sendStatus(500);
         })
-    })
-
-    .catch((err)=> {
-        console.log('error in PUT /api/topic/toggleFeatured');
-        res.sendStatus(500); 
-    })
 })
 
 //WRITTEN BY ATTICUS
 //DELETES SELECTED TOPIC
 router.delete('/deleteTopic/:id', (req, res) => {
-    let topicId = req.params.id; 
+    let topicId = req.params.id;
     console.log('in /api/topics/deleteTopic', topicId);
     let queryText = `DELETE from topic WHERE id = $1;`
     pool.query(queryText, [topicId])
-    .then((result)=> {
-        console.log('successful DELETE /api/topic/deleteTopic');
-        res.sendStatus(200);
-    })
-    .catch((err)=> {
-        console.log('error in DELETE /api/topic/deleteTopic');
-        res.sendStatus(500); 
-    })
+        .then((result) => {
+            console.log('successful DELETE /api/topic/deleteTopic');
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('error in DELETE /api/topic/deleteTopic');
+            res.sendStatus(500);
+        })
 })
 
 
-//WRITTEN BY ATTICUS
 //FETCHES SELCTED TOPICS INFO TO POPULATE TOPICEDIT PAGE (BASED ON URL)
 router.get(`/fetchEditTopicInfo/:id`, (req, res) => {
-    let topicId = req.params.id
+    let topicId = req.params.id;
+    let selectedTopicToSend = {};
+    let contributor1Id = '';
+    let contributor2Id = '';
+
     console.log('in /api/topics/editTopicInfo, ID:', topicId);
-    let queryText = `SELECT topic.topic_title, topic.archive_summary, topic.premise, topic.common_ground, 
-                    "topic"."id" as "topic.id", 
-                    "contributor"."id" as "contributor.id", 
-                    "key_claim"."id" as "key_claim.id", 
-                    key_claim.claim, key_claim.claim_order, contributor.first_name, 
-                    contributor.last_name, contributor.bio, proposal.proposal, 
-                    "stream"."text" as "stream.text", stream.evidence
-                    FROM key_claim 
-                    JOIN topic ON key_claim.topic_id = topic.id 
-                    JOIN contributor ON key_claim.contributor_id = contributor.id
-                    JOIN proposal ON proposal.contributor_id = contributor.id
-                    JOIN stream ON stream.contributor_id = contributor.id 
-                    WHERE topic.id = $1;`; 
+    let queryText = `SELECT topic.topic_title, topic.archive_summary, topic.premise, topic.common_ground, contributor1_id, contributor2_id,
+    "topic"."id" as "topic_id" FROM topic 
+    WHERE topic.id = $1;`;
     pool.query(queryText, [topicId])
-    .then((result) => {
-        console.log('successful GET in /api/topics/editTopicInfo result: ', result.rows);
-        res.send(result.rows)
-    })
-    .catch((err)=> {
-        console.log('error in DELETE /api/topics/editTopicInfo', err);
-        res.sendStatus(500); 
-    })
-    
+        .then((result) => {
+            //Package Topic in selectedTopicToSend
+            selectedTopicToSend = {
+                topicTitle: result.rows[0].topic_title,
+                topicSummary: result.rows[0].archive_summary,
+                topicPremise: result.rows[0].premise,
+                topicReadMore: '',
+                topicCommonGround: result.rows[0].common_ground
+            };
+            contributor1Id = result.rows[0].contributor1_id;
+            contributor2Id = result.rows[0].contributor2_id;
+
+            let queryText = `SELECT id, first_name, last_name, bio, photo_url from contributor where id = $1 OR id = $2;
+            ;`;
+            pool.query(queryText, [contributor1Id, contributor2Id])
+                .then((result) => {
+                    let contributorsToSend 
+
+                    // console.log('successful GET CONTRIBUTORS in /api/topics/editTopicInfo result: ', result.rows);
+                    selectedTopicToSend = {...selectedTopicToSend, contributorsToSend};
+                    res.send(selectedTopicToSend)
+                })
+                .catch((err) => {
+                    console.log('error in GET /api/topics/editTopicInfo', err);
+                    res.sendStatus(500);
+                })
+
+        })
+        .catch((err) => {
+            console.log('error in GET /api/topics/editTopicInfo', err);
+            res.sendStatus(500);
+        })
+
+
 })
 
 
