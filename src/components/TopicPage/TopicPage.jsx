1d652a59f71c284a1f8ecbf2c3f5ede3edaebf9e
Justin <jmpeterson9891@gmail.com>
2018-06-01 14:37:21 -0500

    Completed adding topic id to this.fetchTopicPageContent in componentDidMount in TopicPage.jsx

--- src/components/TopicPage/TopicPage.jsx
+++ src/components/TopicPage/TopicPage.jsx
@@ -33,7 +33,7 @@ export class TopicPage extends Component {
 
   componentDidMount() {
     // this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
-    this.fetchTopicPageContent(this.props.state.landing.featuredLandingPage.id);
+    this.fetchTopicPageContent(this.props.state.landing.featuredLandingPage[0].id);
 
   }
 
