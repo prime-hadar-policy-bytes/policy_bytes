f2d04637140c83d2cc818277ddd0d87140fabccf
Atticus Pomerantz <rapomerantz@gmail.com>
2018-06-02 12:29:54 -0500

    done with topic manage style

--- src/components/Admin/TopicManage/TopicManageModal.jsx
+++ src/components/Admin/TopicManage/TopicManageModal.jsx
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-import { Button, Modal } from 'react-bootstrap'; 
+// import { Button, Modal } from 'react-bootstrap'; 
 
 
-class TopicManageModal extends Component {
-    render() {
-        return(
-            <div>
-            <Modal.Dialog >
-                <Modal.Header bsStyle="danger">
-                <Modal.Title>Are you sure you want to delete this topic?</Modal.Title>
-                </Modal.Header>
+// class TopicManageModal extends Component {
+//     render() {
+//         return(
+//             <div>
+//             <Modal.Dialog >
+//                 <Modal.Header bsStyle="danger">
+//                 <Modal.Title>Are you sure you want to delete this topic?</Modal.Title>
+//                 </Modal.Header>
         
-                <Modal.Body>This action cannot be undone</Modal.Body>
+//                 <Modal.Body>This action cannot be undone</Modal.Body>
         
-                <Modal.Footer>
-                <Button onClick={this.props.handleDismiss}>Close</Button>
-                <Button onClick={this.props.deleteTopic} bsStyle="danger">Yes, Delete This Topic</Button>
-                </Modal.Footer>
-            </Modal.Dialog>
-            </div>
-        )
-    }
-}
+//                 <Modal.Footer>
+//                 <Button onClick={this.props.handleDismiss}>Close</Button>
+//                 <Button onClick={this.props.deleteTopic} bsStyle="danger">Yes, Delete This Topic</Button>
+//                 </Modal.Footer>
+//             </Modal.Dialog>
+//             </div>
+//         )
+//     }
+// }
 
 
-export default TopicManageModal;
+// export default TopicManageModal;
